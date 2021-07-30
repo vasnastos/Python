660adf1d3b477f01d87d3f146b5252c5b23f0960
NASTOS VASILEIOS <63415169+vasnastos@users.noreply.github.com>
2021-07-30 16:48:16 +0300

    Delete tutorial directory

--- tutorial/tut5.py
+++
@@ -1,7 +0,0 @@
-#Print Passo Random Times
-import random as r
-def printPasso(times):
-    x='1525\n'*int(times)
-    print(x)
-
-printPasso(r.randint(10,20))
--- tutorial/tut9/car.py
+++
@@ -1,7 +0,0 @@
-class car:
-    def __init__(self,b,m,p):
-        self.brand=b
-        self.model=m
-        self.price=p
-    def __str__(self):
-        return str(self.brand)+'-'+str(self.model)+'-'+str(self.price)
